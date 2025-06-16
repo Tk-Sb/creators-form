fbb7288984c217c22dc926765b19a74bf46590d3
Tarek-Sb <tksb06712@gmail.com>
2025-06-16 20:02:10 +0300

    only google button left

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,9 +28,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" >
       <body className={`${retroFloral.variable} ${simplifica.variable} ${rubik.variable} antialiased`} >
-        {/* <h1 className="font-retro text-[48px] ">
-          From <br /> nothing <br /> courses
-        </h1> */}
         {children}
       </body>
     </html>
