8a85d03e768ca203e5f750d5e7a356802d639f39
Tarek-Sb <tksb06712@gmail.com>
2025-06-18 17:28:56 +0300

    replace png with jpg

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,7 +27,7 @@ export default function Home() {
       });
       setStep("3")
       // Redirect after successful submission
-      window.location.href = '/' // Your custom success page
+      // window.location.href = '/' // Your custom success page
     } catch (error) {
       console.error('Error:', error)
       // Handle error case
@@ -35,11 +35,11 @@ export default function Home() {
   }
 
   const carouselImages = [
-    { id: 1, url: "./images/Artboard 1.png" },
-    { id: 2, url: "./images/Artboard 2.png" },
-    { id: 3, url: "./images/Artboard 3.png" },
-    { id: 4, url: "./images/Artboard 4.png" },
-    { id: 5, url: "./images/Artboard 5.png" },
+    { id: 1, url: "./images/Artboard 1.jpg" },
+    { id: 2, url: "./images/Artboard 2.jpg" },
+    { id: 3, url: "./images/Artboard 3.jpg" },
+    { id: 4, url: "./images/Artboard 4.jpg" },
+    { id: 5, url: "./images/Artboard 5.jpg" },
   ]
 
   return (
@@ -108,7 +108,7 @@ export default function Home() {
                   </div>
                     :
                   <div className="w-full h-fit flex flex-col lg:flex-row-reverse justify-center items-center gap-7 ">
-                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 6.png" alt={`Artboard 6`} />
+                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 6.jpj" alt={`Artboard 6`} />
                     <div className="w-full h-fit text-base font-rubik font-medium text-center ">
                       استغل ضمان ال 7 أيام استرداد كامل المبلغ… إذا طبّقت وما استفدت،  + خصم ال 30% يلي ما لح تكرر !
                     </div>
@@ -120,9 +120,9 @@ export default function Home() {
               <>
                 <div className="w-full h-fit flex flex-col lg:flex-row-reverse justify-center items-center gap-7 ">
                   {selectedOption === 'computer' ?
-                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 8.png" alt={`Artboard 8`} />
+                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 8.jpg" alt={`Artboard 8`} />
                       :
-                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 7.png" alt={`Artboard 7`} />
+                    <img className="w-full max-w-[400px] aspect-[200/250] rounded-lg " src="./images/Artboard 7.jpj" alt={`Artboard 7`} />
                   }
                   <div className="w-full h-fit text-base font-rubik font-medium text-center ">
                     سنقوم بالتواصل معك خلال 24 ساعة 
