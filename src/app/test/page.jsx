bcfc3e8a69d5d1beacb25ab66695e0944fb7afd4
Tarek-Sb <tksb06712@gmail.com>
2025-06-21 14:50:23 +0300

    s

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,10 +1,14 @@
 'use client'
 
 import GoogleEmailCapture from "@/components/google-btn";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function page() {
   const [email, setEmail] = useState('')
+
+  useEffect(() => {
+    console.log(email)
+  }, [email])
 
   return (
     <div>
